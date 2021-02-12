25dd3fb371f2d4100ff40eb44667a4036563d126
st-vincent1 <sebastian.tate.vincent@gmail.com>
2021-02-12 10:00:43 +0000

    Removed ppl

--- train.py
+++ train.py
@@ -382,8 +382,8 @@ def auto_eval(config, vocab, model_F, test_iters, global_step, temperature):
     acc_pos = evaluator.yelp_acc_1(rev_output[1])
     bleu_neg = evaluator.yelp_ref_bleu_0(rev_output[0])
     bleu_pos = evaluator.yelp_ref_bleu_1(rev_output[1])
-    ppl_neg = evaluator.yelp_ppl(rev_output[0])
-    ppl_pos = evaluator.yelp_ppl(rev_output[1])
+    # ppl_neg = evaluator.yelp_ppl(rev_output[0])
+    # ppl_pos = evaluator.yelp_ppl(rev_output[1])
 
     for k in range(5):
         idx = np.random.randint(len(rev_output[0]))
@@ -405,11 +405,12 @@ def auto_eval(config, vocab, model_F, test_iters, global_step, temperature):
         print('[ref ]', ref_text[1][idx])
 
     print('*' * 20, '********', '*' * 20)
-
+    ppl_pos = 1
+    ppl_neg = 1
     print(('[auto_eval] acc_pos: {:.4f} acc_neg: {:.4f} ' + \
           'bleu_pos: {:.4f} bleu_neg: {:.4f} ' + \
           'ppl_pos: {:.4f} ppl_neg: {:.4f}\n').format(
-              acc_pos, acc_neg, bleu_pos, bleu_neg, ppl_pos, ppl_neg,
+              acc_pos, acc_neg, bleu_pos, bleu_neg, #ppl_pos, ppl_neg,
     ))
 
     
